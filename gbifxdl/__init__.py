73be5b9cfed630fa89edb2244aed365ed38feb78
GuillaumeMougeot <guillaume.mougeot@laposte.net>
2025-01-07 10:55:48 +0100

    defined an __all__ variable

--- gbifxdl/__init__.py
+++ gbifxdl/__init__.py
@@ -6,15 +6,8 @@ except PackageNotFoundError:
     # package is not installed
     pass
 
-from .gbifxdl import (
-    post,
-    config_post,
-    download_occurrences,
-    config_download_occurrences,
-    preprocess_occurrences,
-    config_preprocess_occurrences,
-    preprocess_occurrences_stream,
-    config_preprocess_occurrences_stream,
-)
+__all__ = ["gbifxdl", "crop_img"]
+
+from .gbifxdl import *
 
 from .crop_img import Cropper
--- gbifxdl/gbifxdl.py
+++ gbifxdl/gbifxdl.py
@@ -50,6 +50,15 @@ from collections import defaultdict
 import mmh3
 import psutil
 
+__all__ = ["post",
+    "config_post",
+    "download_occurrences",
+    "config_download_occurrences",
+    "preprocess_occurrences",
+    "config_preprocess_occurrences",
+    "preprocess_occurrences_stream",
+    "config_preprocess_occurrences_stream",]
+
 # -----------------------------------------------------------------------------
 # Logger
 
