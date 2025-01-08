69b5f48211838cc789f28dd4af7bc96746c616f3
GuillaumeMougeot <guillaume.mougeot@laposte.net>
2025-01-08 10:21:58 +0100

    complete example2

--- examples/example2_download_occurrence.py
+++ examples/example2_download_occurrence.py
@@ -4,18 +4,31 @@
 # the occurrence file. The file can then be preprocessed
 # and transformed into a Parquet file.
 
-from gbifxdl import poll_status, download_occurrences
+from gbifxdl import (
+    poll_status,
+    download_occurrences,
+    preprocess_occurrences_stream)
 
 # Replace with your own download key
 download_key = "0060185-241126133413365"
 
 # Poll the POST status and wait if not ready to be downloaded
-# status = 
+status = poll_status(download_key)
 
 # Download the GBIF file
-download_path = download_occurrences(
-    download_key= download_key,
-    dataset_dir = 'data/classif/lepi_small',
-    file_format = 'dwca'
-)
+if status == 'succeeded':
+    download_path = download_occurrences(
+        download_key= download_key,
+        dataset_dir = 'data/classif/lepi_small',
+        file_format = 'dwca'
+    )
+else:
+    print(f"Download failed because status is {status}.")
+    download_path = None
+
+# Preprocess the downloaded file
+if download_path is not None:
+    preprocessed_path = preprocess_occurrences_stream(occurrences_path=download_path)
+
+print(f"Preprocessed file is located here: {preprocessed_path}")
 
